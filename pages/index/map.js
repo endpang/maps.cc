161a5f7b503780b991ab95c1e48bf5b5113cff03
endler <zhiweip@oupeng.com>
2018-03-22 18:45:45 +0800

    add interval

--- pages/index/map.js
+++ pages/index/map.js
@@ -25,53 +25,60 @@ Page({
   },  
   onReady: function (e) {
     // 使用 wx.createMapContext 获取 map 上下文
-    wx.login({
-      success: function (r) {
-        var code = r.code;//登录凭证
-        if (code) {
-          //2、调用获取用户信息接口
-          wx.getUserInfo({
-            success: function (res) {
-              console.log({ encryptedData: res.encryptedData, iv: res.iv, code: code })
-              //3.请求自己的服务器，解密用户信息 获取unionId等加密信息
-              wx.request({
-                url: 'https://maps.cc/wx/test.php',//自己的服务接口地址
-                method: 'post',
-                header: {
-                  'content-type': 'application/x-www-form-urlencoded'
-                },
-                data: { encryptedData: res.encryptedData, iv: res.iv, code: code },
-                success: function (data) {
-                  console.log(data)
-                  //4.解密成功后 获取自己服务器返回的结果
-                  if (data.data.status == 1) {
-                    var userInfo_ = data.data.userInfo;
-                    app.globalData.openid = data.data.userInfo.openid
-                    console.log(app.globalData.userInfo, app.globalData.openid)
-                  } else {
-                    console.log('解密失败')
+    if (app.globalData.openid){
+
+    }else{
+
+      wx.login({
+        success: function (r) {
+          var code = r.code;//登录凭证
+          if (code) {
+            //2、调用获取用户信息接口
+            wx.getUserInfo({
+              success: function (res) {
+                console.log({ encryptedData: res.encryptedData, iv: res.iv, code: code })
+                //3.请求自己的服务器，解密用户信息 获取unionId等加密信息
+                wx.request({
+                  url: 'https://maps.cc/wx/test.php',//自己的服务接口地址
+                  method: 'post',
+                  header: {
+                    'content-type': 'application/x-www-form-urlencoded'
+                  },
+                  data: { encryptedData: res.encryptedData, iv: res.iv, code: code },
+                  success: function (data) {
+                    console.log(data)
+                    //4.解密成功后 获取自己服务器返回的结果
+                    if (data.data.status == 1) {
+                      var userInfo_ = data.data.userInfo;
+                      app.globalData.openid = data.data.userInfo.openid
+                      console.log(app.globalData.userInfo, app.globalData.openid)
+                    } else {
+                      console.log('解密失败')
+                    }
+
+                  },
+                  fail: function () {
+                    console.log('系统错误')
                   }
+                })
+              },
+              fail: function () {
+                console.log('获取用户信息失败')
+              }
+            })
 
-                },
-                fail: function () {
-                  console.log('系统错误')
-                }
-              })
-            },
-            fail: function () {
-              console.log('获取用户信息失败')
-            }
-          })
-
-        } else {
-          console.log('获取用户登录态失败！' + r.errMsg)
+          } else {
+            console.log('获取用户登录态失败！' + r.errMsg)
+          }
+        },
+        fail: function () {
+          console.log('登陆失败')
         }
-      },
-      fail: function () {
-        console.log('登陆失败')
-      }
-    })
+      })
 
+    }
+  
+    //地图初始化
     wx.downloadFile({
       url: 'https://maps.cc/345.png',
       type: 'audio',
@@ -103,6 +110,16 @@ Page({
       this.getCenterLocation()
 
     }.bind(this), 1000)
+    setTimeout(function () {
+      //要延时执行的代码
+      setInterval(function () {
+
+        console.log("tt")
+         
+      }.bind(this), 10000) //循环时间 这里是1秒   
+
+    }.bind(this), 2000)
+     
     
   },
   getCenterLocation: function () {
